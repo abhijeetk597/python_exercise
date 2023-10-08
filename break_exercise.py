585d1457cc5aef348e075a98a64c97dbe0f791b6
abhijeetk597 <138308825+abhijeetk597@users.noreply.github.com>
2023-10-09 05:08:47 +0530

    Update break_exercise.py

--- break_exercise.py
+++ break_exercise.py
@@ -1,13 +1,16 @@
 """
+===========================================================================================
 # 1. Write a Python program to find the first occurrence of a number in a list using a for loop and break statement.
+------------------------------------
 list = [88, 97, 87, 92, 97, 73, 60, 69, 61, 90, 74, 80, 70, 92, 96, 73, 73, 76, 74, 89, 60, 67, 62, 54, 53, 61, 61, 57, 82, 58, 60, 75, 84, 51, 74, 86, 65, 65, 90, 61, 59, 84, 74, 66, 69, 79, 91, 97, 51, 72, 94, 87, 52, 91, 94, 54, 78, 84, 59, 80, 55, 97, 57, 51, 50, 65, 82, 50, 90, 52, 95, 96, 66, 71, 54, 74, 93, 95, 91, 68, 82, 91, 95, 76, 85, 70, 96, 64, 55, 92, 67, 77, 93, 72, 75, 62, 72, 63, 92, 95]
 num = 80
 for i in range(len(list)):
     if list[i] == num:
         print('Index of first occurrence is: ',i)
         break
-
+===========================================================================================
 # 2. Write a Python program to search for a specific element in a list using a while loop and break statement.
+------------------------------------
 def find_element(n, list):
     i = 0
     while i < len(list):
@@ -22,8 +25,9 @@ def find_element(n, list):
 list = [88, 97, 87, 92, 97, 73, 60, 69, 61, 90, 74, 80, 70, 92, 96, 73, 73, 76, 74, 89, 60, 67, 62, 54, 53, 61, 61, 57, 82, 58, 60, 75, 84, 51, 74, 86, 65, 65, 90, 61, 59, 84, 74, 66, 69, 79, 91, 97, 51, 72, 94, 87, 52, 91, 94, 54, 78, 84, 59, 80, 55, 97, 57, 51, 50, 65, 82, 50, 90, 52, 95, 96, 66, 71, 54, 74, 93, 95, 91, 68, 82, 91, 95, 76, 85, 70, 96, 64, 55, 92, 67, 77, 93, 72, 75, 62, 72, 63, 92, 95]
 num = int(input('Enter number to search in list: '))
 find_element(num, list)
-
+===========================================================================================
 # 3. Write a Python program to find the prime numbers between 1 and 100 using a for loop and break statement.
+------------------------------------
 primelist = []
 for i in range(1,101):
     if i == 1:
@@ -37,8 +41,9 @@ for i in range(1,101):
     if j == i-1:
         primelist.append(i)
 print(primelist)
-
+===========================================================================================
 # 4. Write a Python program to check if a number is present in a list using a while loop and break statement.
+------------------------------------
 def find_element(n, list):
     i = 0
     while i < len(list):
@@ -53,8 +58,9 @@ def find_element(n, list):
 list = [88, 97, 87, 92, 97, 73, 60, 69, 61, 90, 74, 80, 70, 92, 96, 73, 73, 76, 74, 89, 60, 67, 62, 54, 53, 61, 61, 57, 82, 58, 60, 75, 84, 51, 74, 86, 65, 65, 90, 61, 59, 84, 74, 66, 69, 79, 91, 97, 51, 72, 94, 87, 52, 91, 94, 54, 78, 84, 59, 80, 55, 97, 57, 51, 50, 65, 82, 50, 90, 52, 95, 96, 66, 71, 54, 74, 93, 95, 91, 68, 82, 91, 95, 76, 85, 70, 96, 64, 55, 92, 67, 77, 93, 72, 75, 62, 72, 63, 92, 95]
 num = int(input('Enter number to search in list: '))
 find_element(num, list)
-
+===========================================================================================
 # 5. Write a Python program to find the largest palindrome number in a given range using a for loop and break statement.
+------------------------------------
 def is_palindrome(n):
     if str(n) == str(n)[::-1]:
         return 1
@@ -65,8 +71,9 @@ for i in range(2000,3000):
         if i > palindrome:
             palindrome = i
 print('Largest Palindrome in given range is: ', palindrome)
-
+===========================================================================================
 # 6. Write a Python program to find the first negative number in a list using a while loop and break statement.
+------------------------------------
 list = [88, 97, 87, 92, 97, 73, 60, -15, 69, 61, 90, 74, 80, 70, -10, 92, 96, 73, 73, 76, 74, 89, 60, 67, 62]
 i = 0
 while i < len(list):
@@ -74,17 +81,19 @@ while i < len(list):
         print('First negative number in the list is: ', list[i])
         break
     i += 1
-
+===========================================================================================
 # 7. Write a Python program to print the elements of a list until a specific condition
 # is met using a for loop and break statement.
+------------------------------------
 list = [88, 97, 87, 92, 97, 73, 60, 69, 61, 90, 74, 80, 70, 92, 96, 73, 73, 76, 74, 89, 60, 67, 62]
 for i in list:
     print(i)
     if i % 7 == 3:  #specific condition
         print('Loop broken as per condition')
         break
-
+===========================================================================================
 # 8. Write a Python program to search for a specific character in a string using a while loop and break statement.
+------------------------------------
 string = 'India is my country. All indians are my brothers and sisters.'
 char = input('Enter char to search: ')[0]
 i = 0
@@ -95,16 +104,18 @@ while i < len(string):
     i += 1
 else:
     print(f'Char {char} is not present in given string.')
-
+===========================================================================================
 # 9. Write a Python program to find the first occurrence of a vowel in a string using a for loop and break statement.
+------------------------------------
 string = 'fkstoccurrence specific character'
 for i in string.lower():
     if i in ['a','e','i','o','u']:
         print('Vowel is present at index',string.lower().index(i))
         break
-
+===========================================================================================
 # 10. Write a Python program to find the index of the first occurrence
 # of a substring in a string using a while loop and break statement.
+------------------------------------
 def search_substring(string, substr):
     x =len(substr)
     index = -1
@@ -123,5 +134,5 @@ if res != -1:
     print(f'Substring present at index {res}')
 else:
     print('Substring not present.')
-
+===========================================================================================
 """
