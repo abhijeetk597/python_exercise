7b0a1dc3b2df2d79bed152d80af94eb982b24af0
abhijeetk597 <138308825+abhijeetk597@users.noreply.github.com>
2023-10-09 05:23:39 +0530

    Update if_else_exercise.py

--- if_else_exercise.py
+++ if_else_exercise.py
@@ -1,5 +1,7 @@
 """
+---------------------------------------
 # 1. Write a Python program to check if a number is positive, negative, or zero.
+---------------------------------------
 num = int(input('Enter number to check: '))
 if num < 0:
     print('Entered Number', num, 'is negative.')
@@ -8,11 +10,15 @@ elif num > 0:
 else:
     print('Entered Number is zero.')
 
+---------------------------------------
 # 2. Write a Python program to check if a number is even or odd.
+---------------------------------------
 num = int(input('Enter number to check if even or odd: '))
 print('Entered number is even' if num % 2== 0 else 'Entered number is odd')
 
+---------------------------------------
 # 3. Write a Python program to check if a year is a leap year or not.
+---------------------------------------
 num = int(input('Enter year: '))
 if num % 400 == 0:
     print(num, 'is a leap year.')
@@ -21,7 +27,9 @@ elif num % 4 == 0 and num % 100 != 0:
 else:
     print(num, 'is not a leap year.')
 
+---------------------------------------
 # 4. Write a Python program to find the maximum of three numbers using if-else.
+---------------------------------------
 x,y,z = map(int,input('Enter 3 numbers (space separated): ').split())
 max = x
 if y > x:
@@ -30,7 +38,9 @@ if z >= y and z > x:
     max = z
 print('Maximum number is: ',max)
 
+---------------------------------------
 # 5. Write a Python program to check if a number is prime.
+---------------------------------------
 def isPrime(n):
     if n < 2:
         return 'Entered number is not Prime'
@@ -45,14 +55,18 @@ def isPrime(n):
 num = int(input('Enter number: '))
 print(isPrime(num))
 
+---------------------------------------
 # 6. Write a Python program to check if a number is divisible by both 3 and 5.
+---------------------------------------
 num = int(input('Enter number: '))
 if num % 3 == 0 and num % 5 == 0:
     print('Entered number is divisible by both 3 and 5')
 else:
     print('Entered number is not divisible by both 3 and 5')
 
+---------------------------------------
 # 7. Write a Python program to check if a character is a vowel or consonant.
+---------------------------------------
 char = input('Enter character: ')
 char = char.lower()
 if char.isalpha():
@@ -63,14 +77,18 @@ if char.isalpha():
 else:
     print('Entered character is not not an alphabet.')
 
+---------------------------------------
 # 8. Write a Python program to check if a given string is a palindrome using if-else.
+---------------------------------------
 string = input('Enter string: ')
 if string == string[::-1]:
     print('Entered string is palindrome')
 else:
     print('Entered string is not palindrome')
 
+---------------------------------------
 # 9. Write a Python program to determine the largest among three numbers using nested if-else.
+---------------------------------------
 x,y,z = map(int,input('Enter 3 numbers (space separated): ').split())
 max = x
 if y > x:
@@ -85,8 +103,10 @@ else:
         max = z
 print('Maximum number is: ',max)
 
+---------------------------------------
 # 10. Write a Python program to check if a triangle is equilateral,
 # isosceles, or scalene based on its side lengths using if-else.
+---------------------------------------
 x,y,z = map(int,input('Enter 3 sides of triangle (space separated): ').split())
 if x < y + z and y < x + z and z < x + y:
     if x == y == z:
